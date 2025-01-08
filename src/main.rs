83de348c9419b1a748582cc2dbb3ee23482769d2
Neil Gardner <neilgardner1963@gmail.com>
2025-01-08 01:21:14 +0000

    added MongoDB support and query filter

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
 use axum::{
-  extract::DefaultBodyLimit, http::Method, routing::{get, post, put}, Router
+    extract::DefaultBodyLimit,
+    http::Method,
+    routing::{get, post, put},
+Router,
 };
-// use mongodb::action::StartTransaction;
-use tower_http::cors::{CorsLayer, Any};
+use tower_http::cors::{Any, CorsLayer};
 
-
+mod db;
 mod files;
 mod options;
 mod routes;
-// mod db;
 
 use routes::*;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-  let cors = CorsLayer::new()
-  .allow_origin(Any)
-  .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE]);
+    let cors = CorsLayer::new().allow_origin(Any).allow_methods(vec![
+        Method::GET,
+        Method::POST,
+        Method::PUT,
+        Method::DELETE,
+    ]);
 
-  let app = Router::new()
-    .route("/", get(welcome))
-      .route("/upload", post(upload_asset))
-      .route("/process", put(process_asset))
-      .route("/check-file/:file_name", get(check_file))
-      // The default axum body size limit is 2MiB, so we increase it to 1GiB.
-      .layer(DefaultBodyLimit::max(1024 * 1024 * 1024))
-      .layer(cors)
-      .into_make_service();
-  let ip = dotenv::var("LOCAL_ADDRESS").unwrap_or(String::from("0.0.0.0"));
-  let port = dotenv::var("PORT").unwrap_or(String::from("3000"));
-  let address = format!("{}:{}", ip, port);
-  let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
-  axum::serve(listener, app).await.unwrap();
-  Ok(())
+    let app = Router::new()
+        .route("/", get(welcome))
+        .route("/upload", post(upload_asset))
+        .route("/process", put(process_asset))
+        .route("/check-file/:file_name", get(check_file))
+        .route("/dataset/:id", get(get_dataset))
+        // The default axum body size limit is 2MiB, so we increase it to 1GiB.
+        .layer(DefaultBodyLimit::max(1024 * 1024 * 1024))
+        .layer(cors)
+        .into_make_service();
+    let ip = dotenv::var("LOCAL_ADDRESS").unwrap_or(String::from("0.0.0.0"));
+    let port = dotenv::var("PORT").unwrap_or(String::from("3000"));
+    let address = format!("{}:{}", ip, port);
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
--- src/options.rs
+++ src/options.rs
@@ -1,6 +1,9 @@
 
+use std::str::FromStr;
+use bson::{doc, oid::ObjectId, Document};
 use serde::{Deserialize, Serialize};
 use axum_typed_multipart::{FieldData, TryFromMultipart};
+use spreadsheet_to_json::simple_string_patterns::ToSegments;
 use tempfile::NamedTempFile;
 
 #[derive(TryFromMultipart, Debug)]
@@ -25,6 +28,32 @@ pub struct CoreOptions {
   pub cols: Option<String>,
   pub sheet_index: Option<usize>,
   pub header_index: Option<usize>,
+  pub dataset_id: Option<String>,
+  pub import_id: Option<String>,
+}
+
+impl CoreOptions {
+  pub fn to_json_value(&self) -> serde_json::Value {
+    let mode_str = self.mode.clone().unwrap_or("sync".to_string());
+    let mut value = serde_json::json!({
+      "filename": self.filename.clone().unwrap_or_default(),
+      "sheet_index": self.sheet_index.unwrap_or(0),
+      "header_index": self.header_index.unwrap_or(0),
+      "mode": mode_str,
+      "lines": self.lines.unwrap_or(0) > 0,
+    });
+    if let Some(keys) = self.keys.clone() {
+      if keys.len() > 0 {
+        value["keys"] = serde_json::json!(keys.to_parts(","));
+      }
+    }
+    if let Some(cols) = self.cols.clone() {
+      if cols.len() > 0 {
+        value["columns"] = serde_json::json!(cols.to_parts(","));
+      }
+    }
+    value
+  }
 }
 
 impl UploadAssetRequest {
@@ -38,6 +67,82 @@ impl UploadAssetRequest {
       cols: self.cols.clone(),
       sheet_index: self.sheet_index,
       header_index: self.header_index,
+      dataset_id: None,
+      import_id: None,
     }
   }
+}
+
+pub enum DataSetMatcher {
+  NameIndex(String, u32),
+  Id(String),
+}
+
+impl DataSetMatcher {
+  pub fn from_name_index(name: &str, index: u32) -> Self {
+    DataSetMatcher::NameIndex(name.to_string(), index)
+  }
+
+  pub fn from_id(id: &str) -> Self {
+    DataSetMatcher::Id(id.to_string())
+  }
+
+  pub fn to_criteria(&self) -> Document {
+    match self {
+      DataSetMatcher::NameIndex(name, index) => {
+        doc! {
+          "name": name,
+          "index": index,
+        }
+      },
+      DataSetMatcher::Id(id) => {
+        doc! {
+          "_id": ObjectId::from_str(id).ok(),
+        }
+      }
+
+    }
+
+  }
+}
+
+
+#[derive(Deserialize)]
+pub struct QueryFilterParams {
+    pub f: Option<String>,
+    pub v: Option<String>,
+    pub o: Option<String>,
+    pub start: Option<u64>,
+    pub limit: Option<u64>,
+}
+
+impl QueryFilterParams {
+    pub fn to_criteria(&self) -> Option<Document> {
+        let mut criteria = doc! {};
+        if let Some(field) = self.f.clone() {
+            if let Some(value) = self.v.clone() {
+                let operator = self.o.clone().unwrap_or("eq".to_string());
+                let value = match operator.as_str() {
+                    "ne" => doc! { "$ne": value },
+                    "gt" => doc! { "$gt": value },
+                    "gte" => doc! { "$gte": value },
+                    "lt" => doc! { "$lt": value },
+                    "lte" => doc! { "$lte": value },
+                    "in" => doc! { "$in": value.to_parts(",") },
+                    "nin" => doc! { "$nin": value.to_parts(",") },
+                    "r" | "regex" => doc! { "$regex": value, "$options": "i" },
+                    "starts" | "starts_with" => doc! { "$regex": format!("^{}",value.trim()), "$options": "i" },
+                    "ends" | "ends_with" => doc! { "$regex": format!("{}$",value.trim()), "$options": "i" },
+                    _ => doc! { "$eq": value },
+                };
+                criteria = doc! { format!("data.{}", field): value };
+            }
+        }
+        if criteria.is_empty() {
+            None
+        } else {
+            Some(criteria)
+        }
+    }
+    
 }
